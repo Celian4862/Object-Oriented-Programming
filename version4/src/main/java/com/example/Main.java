2c0315bac710fdc4d861246c02b5bdcf47298593
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2024-11-13 18:45:24 +0800

    Removed the commented line from all Main.java files

--- version4/src/main/java/com/example/Main.java
+++ version4/src/main/java/com/example/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         EmployeeRoster roster = new EmployeeRoster(10);
         roster.addEmployee(new HourlyEmployee(1, new Name("Doe", "John"), new Date(1997, 1, 15), new Date(2024, 5, 10), 12.3f, 1.5));
-        // roster.addEmployee(new HourlyEmployee(1, new Name("Doe", "John"), new Date(1997, 1, 15), new Date(2024, 5, 10), 12.3f, 1.5));
         roster.addEmployee(new CommissionEmployee(2, new Name("Doe", "John"), new Date(1997, 1, 15), new Date(2024, 5, 10), 100.15));
         roster.addEmployee(new BasePlusCommissionEmployee(3, new Name("Doe", "John", "Pear"), 100.2, 50.0));
         roster.displayAllEmployees();
